620d4d8b226ea9e8d0da5b623b75b70b81aecb73
Valdas Žemaitis <valdas@idega.com>
2017-08-24 09:34:35 +0300

    Providing init params for authentication servlet; bean classes

---
+++ src/java/com/idega/block/oauth2/server/authentication/bean/AccessToken.java
@@ -0,0 +1,48 @@
+package com.idega.block.oauth2.server.authentication.bean;
+
+import java.io.Serializable;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+
+import com.idega.block.login.bean.OAuthToken;
+
+@XmlRootElement
+@XmlAccessorType(XmlAccessType.FIELD)
+public class AccessToken implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private OAuthToken token;
+
+	private String user;
+
+	public AccessToken() {
+		super();
+	}
+
+	public AccessToken(OAuthToken token, String user) {
+		super();
+
+		this.token = token;
+		this.user = user;
+	}
+
+	public OAuthToken getToken() {
+		return token;
+	}
+
+	public void setToken(OAuthToken token) {
+		this.token = token;
+	}
+
+	public String getUser() {
+		return user;
+	}
+
+	public void setUser(String user) {
+		this.user = user;
+	}
+
+}
--- src/java/com/idega/block/oauth2/server/authentication/bean/User.java
+++ src/java/com/idega/block/oauth2/server/authentication/bean/User.java
@@ -2,9 +2,17 @@ package com.idega.block.oauth2.server.authentication.bean;
 
 import java.io.Serializable;
 
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+@XmlAccessorType(XmlAccessType.FIELD)
 public class User implements Serializable {
 
 	private static final long serialVersionUID = 3109303153488234141L;
+
+	private AccessToken accessToken;
 
 	private String name;
 
@@ -14,6 +22,14 @@ public class User implements Serializable {
 
 	public void setName(String name) {
 		this.name = name;
+	}
+
+	public AccessToken getAccessToken() {
+		return accessToken;
+	}
+
+	public void setAccessToken(AccessToken accessToken) {
+		this.accessToken = accessToken;
 	}
 
 }
