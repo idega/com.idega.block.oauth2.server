880db099d0fd79d541bb15ca379bf0c4ec6a0480
Valdas Žemaitis <valdas@idega.com>
2019-07-16 14:34:14 +0300

    By default caching OAuth tokens

--- src/java/com/idega/block/oauth2/server/IWBundleStarter.java
+++ src/java/com/idega/block/oauth2/server/IWBundleStarter.java
@@ -3,84 +3,87 @@
  *
  * Idega Software hf. Source Code Licence Agreement x
  *
- * This agreement, made this 10th of February 2006 by and between 
- * Idega Software hf., a business formed and operating under laws 
- * of Iceland, having its principal place of business in Reykjavik, 
- * Iceland, hereinafter after referred to as "Manufacturer" and Agura 
+ * This agreement, made this 10th of February 2006 by and between
+ * Idega Software hf., a business formed and operating under laws
+ * of Iceland, having its principal place of business in Reykjavik,
+ * Iceland, hereinafter after referred to as "Manufacturer" and Agura
  * IT hereinafter referred to as "Licensee".
- * 1.  License Grant: Upon completion of this agreement, the source 
- *     code that may be made available according to the documentation for 
- *     a particular software product (Software) from Manufacturer 
- *     (Source Code) shall be provided to Licensee, provided that 
- *     (1) funds have been received for payment of the License for Software and 
- *     (2) the appropriate License has been purchased as stated in the 
- *     documentation for Software. As used in this License Agreement, 
- *     Licensee shall also mean the individual using or installing 
- *     the source code together with any individual or entity, including 
- *     but not limited to your employer, on whose behalf you are acting 
- *     in using or installing the Source Code. By completing this agreement, 
- *     Licensee agrees to be bound by the terms and conditions of this Source 
- *     Code License Agreement. This Source Code License Agreement shall 
- *     be an extension of the Software License Agreement for the associated 
- *     product. No additional amendment or modification shall be made 
- *     to this Agreement except in writing signed by Licensee and 
+ * 1.  License Grant: Upon completion of this agreement, the source
+ *     code that may be made available according to the documentation for
+ *     a particular software product (Software) from Manufacturer
+ *     (Source Code) shall be provided to Licensee, provided that
+ *     (1) funds have been received for payment of the License for Software and
+ *     (2) the appropriate License has been purchased as stated in the
+ *     documentation for Software. As used in this License Agreement,
+ *     Licensee shall also mean the individual using or installing
+ *     the source code together with any individual or entity, including
+ *     but not limited to your employer, on whose behalf you are acting
+ *     in using or installing the Source Code. By completing this agreement,
+ *     Licensee agrees to be bound by the terms and conditions of this Source
+ *     Code License Agreement. This Source Code License Agreement shall
+ *     be an extension of the Software License Agreement for the associated
+ *     product. No additional amendment or modification shall be made
+ *     to this Agreement except in writing signed by Licensee and
  *     Manufacturer. This Agreement is effective indefinitely and once
- *     completed, cannot be terminated. Manufacturer hereby grants to 
- *     Licensee a non-transferable, worldwide license during the term of 
- *     this Agreement to use the Source Code for the associated product 
- *     purchased. In the event the Software License Agreement to the 
- *     associated product is terminated; (1) Licensee's rights to use 
- *     the Source Code are revoked and (2) Licensee shall destroy all 
- *     copies of the Source Code including any Source Code used in 
+ *     completed, cannot be terminated. Manufacturer hereby grants to
+ *     Licensee a non-transferable, worldwide license during the term of
+ *     this Agreement to use the Source Code for the associated product
+ *     purchased. In the event the Software License Agreement to the
+ *     associated product is terminated; (1) Licensee's rights to use
+ *     the Source Code are revoked and (2) Licensee shall destroy all
+ *     copies of the Source Code including any Source Code used in
  *     Licensee's applications.
  * 2.  License Limitations
- *     2.1 Licensee may not resell, rent, lease or distribute the 
- *         Source Code alone, it shall only be distributed as a 
+ *     2.1 Licensee may not resell, rent, lease or distribute the
+ *         Source Code alone, it shall only be distributed as a
  *         compiled component of an application.
- *     2.2 Licensee shall protect and keep secure all Source Code 
- *         provided by this this Source Code License Agreement. 
- *         All Source Code provided by this Agreement that is used 
+ *     2.2 Licensee shall protect and keep secure all Source Code
+ *         provided by this this Source Code License Agreement.
+ *         All Source Code provided by this Agreement that is used
  *         with an application that is distributed or accessible outside
- *         Licensee's organization (including use from the Internet), 
- *         must be protected to the extent that it cannot be easily 
+ *         Licensee's organization (including use from the Internet),
+ *         must be protected to the extent that it cannot be easily
  *         extracted or decompiled.
- *     2.3 The Licensee shall not resell, rent, lease or distribute 
- *         the products created from the Source Code in any way that 
+ *     2.3 The Licensee shall not resell, rent, lease or distribute
+ *         the products created from the Source Code in any way that
  *         would compete with Idega Software.
- *     2.4 Manufacturer's copyright notices may not be removed from 
+ *     2.4 Manufacturer's copyright notices may not be removed from
  *         the Source Code.
- *     2.5 All modifications on the source code by Licencee must 
+ *     2.5 All modifications on the source code by Licencee must
  *         be submitted to or provided to Manufacturer.
- * 3.  Copyright: Manufacturer's source code is copyrighted and contains 
- *     proprietary information. Licensee shall not distribute or 
- *     reveal the Source Code to anyone other than the software 
- *     developers of Licensee's organization. Licensee may be held 
- *     legally responsible for any infringement of intellectual property 
- *     rights that is caused or encouraged by Licensee's failure to abide 
- *     by the terms of this Agreement. Licensee may make copies of the 
- *     Source Code provided the copyright and trademark notices are 
- *     reproduced in their entirety on the copy. Manufacturer reserves 
+ * 3.  Copyright: Manufacturer's source code is copyrighted and contains
+ *     proprietary information. Licensee shall not distribute or
+ *     reveal the Source Code to anyone other than the software
+ *     developers of Licensee's organization. Licensee may be held
+ *     legally responsible for any infringement of intellectual property
+ *     rights that is caused or encouraged by Licensee's failure to abide
+ *     by the terms of this Agreement. Licensee may make copies of the
+ *     Source Code provided the copyright and trademark notices are
+ *     reproduced in their entirety on the copy. Manufacturer reserves
  *     all rights not specifically granted to Licensee.
  *
- * 4.  Warranty & Risks: Although efforts have been made to assure that the 
- *     Source Code is correct, reliable, date compliant, and technically 
- *     accurate, the Source Code is licensed to Licensee as is and without 
- *     warranties as to performance of merchantability, fitness for a 
- *     particular purpose or use, or any other warranties whether 
- *     expressed or implied. Licensee's organization and all users 
- *     of the source code assume all risks when using it. The manufacturers, 
- *     distributors and resellers of the Source Code shall not be liable 
- *     for any consequential, incidental, punitive or special damages 
- *     arising out of the use of or inability to use the source code or 
- *     the provision of or failure to provide support services, even if we 
- *     have been advised of the possibility of such damages. In any case, 
- *     the entire liability under any provision of this agreement shall be 
- *     limited to the greater of the amount actually paid by Licensee for the 
- *     Software or 5.00 USD. No returns will be provided for the associated 
- *     License that was purchased to become eligible to receive the Source 
- *     Code after Licensee receives the source code. 
+ * 4.  Warranty & Risks: Although efforts have been made to assure that the
+ *     Source Code is correct, reliable, date compliant, and technically
+ *     accurate, the Source Code is licensed to Licensee as is and without
+ *     warranties as to performance of merchantability, fitness for a
+ *     particular purpose or use, or any other warranties whether
+ *     expressed or implied. Licensee's organization and all users
+ *     of the source code assume all risks when using it. The manufacturers,
+ *     distributors and resellers of the Source Code shall not be liable
+ *     for any consequential, incidental, punitive or special damages
+ *     arising out of the use of or inability to use the source code or
+ *     the provision of or failure to provide support services, even if we
+ *     have been advised of the possibility of such damages. In any case,
+ *     the entire liability under any provision of this agreement shall be
+ *     limited to the greater of the amount actually paid by Licensee for the
+ *     Software or 5.00 USD. No returns will be provided for the associated
+ *     License that was purchased to become eligible to receive the Source
+ *     Code after Licensee receives the source code.
  */
 package com.idega.block.oauth2.server;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -90,18 +93,29 @@ import org.springframework.security.oauth2.provider.client.JdbcClientDetailsServ
 import com.idega.block.oauth2.server.data.dao.OAuth2DAO;
 import com.idega.idegaweb.IWBundle;
 import com.idega.idegaweb.IWBundleStartable;
+import com.idega.idegaweb.IWMainApplication;
+import com.idega.idegaweb.IWMainApplicationSettings;
 import com.idega.util.ListUtil;
 import com.idega.util.expression.ELUtil;
+import com.idega.util.timer.TimerEntry;
+import com.idega.util.timer.TimerListener;
+import com.idega.util.timer.TimerManager;
 
 /**
  * <p>Initial module configuration</p>
- * <p>You can report about problems to: 
+ * <p>You can report about problems to:
  * <a href="mailto:martynas@idega.is">Martynas Stakė</a></p>
  *
  * @version 1.0.0 2015 spal. 28
  * @author <a href="mailto:martynas@idega.is">Martynas Stakė</a>
  */
 public class IWBundleStarter implements IWBundleStartable {
+
+	private static final Logger LOGGER = Logger.getLogger(IWBundleStarter.class.getName());
+
+	private static TimerManager tManager = null;
+
+	private static TimerEntry oauthTokensCacheTimer = null;
 
 	@Autowired(required=false)
 	@Qualifier("clientDetails")
@@ -129,7 +143,10 @@ public class IWBundleStarter implements IWBundleStartable {
 	@Override
 	public void start(IWBundle starterBundle) {
 		getOAuth2DAO().checkTables();
-		
+
+		startOAuthTokensCacheDaemon(starterBundle);
+		getOAuth2DAO().doCacheAccessTokens();
+
 		if (getClientDetailsService() != null && ListUtil.isEmpty(getClientDetailsService().listClientDetails())) {
 			BaseClientDetails details = new BaseClientDetails(
 					"restapp",
@@ -150,16 +167,59 @@ public class IWBundleStarter implements IWBundleStartable {
 			getClientDetailsService().addClientDetails(details);
 
 			details = new BaseClientDetails(
-					"felix_desktop_application", 
-					null, 
-					"read,write,trust", 
-					"password,authorization_code,refresh_token,implicit", 
+					"felix_desktop_application",
+					null,
+					"read,write,trust",
+					"password,authorization_code,refresh_token,implicit",
 					"ROLE_APP");
 			details.setClientSecret("felix_desktop_application");
 			getClientDetailsService().addClientDetails(details);
 		}
 	}
 
+	private void startOAuthTokensCacheDaemon(IWBundle iwb) {
+		IWMainApplication iwma = iwb.getApplication();
+		IWMainApplicationSettings settings = iwma.getSettings();
+		String propName = "oauth.handle_tokens_cache";
+		if (!settings.getBoolean(propName, true)) {
+			return;
+		}
+
+		if (tManager == null) {
+			tManager = new TimerManager();
+		}
+
+		if (oauthTokensCacheTimer == null) {
+			try {
+				oauthTokensCacheTimer = tManager.addTimer(30, 3, -1, -1, -1, -1, new TimerListener() {
+
+					@Override
+					public void handleTimer(TimerEntry entry) {
+						try {
+							if (!settings.getBoolean(propName, true)) {
+								return;
+							}
+
+							LOGGER.info("Starting to handle OAuth 2 tokens cache");
+							getOAuth2DAO().doCacheAccessTokens();
+							LOGGER.info("Finished handling OAuth 2 tokens cache");
+						} catch (Exception e) {
+							LOGGER.log(Level.WARNING, "Error handling OAuth 2 tokens cache", e);
+						}
+					}
+
+				});
+				if (oauthTokensCacheTimer != null) {
+					oauthTokensCacheTimer.setRepetitive(true);
+				}
+			} catch (Exception e) {
+				oauthTokensCacheTimer = null;
+				LOGGER.log(Level.WARNING, "Error starting OAuth 2 tokens cache", e);
+			}
+		}
+	}
+
 	@Override
 	public void stop(IWBundle starterBundle) {}
+
 }
